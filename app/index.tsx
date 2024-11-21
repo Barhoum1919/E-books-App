465544c54f4170392a75d8e83cb85055b3cd4b28
Barhoum1919 <ibrahimdarghouthi1919@gmail.com>
2024-11-21 20:43:00 +0100

    First Commit

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+      <Link href="/home" style={styles.link}>
+        Start Reading!
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  link: {
+    fontSize: 18,
+    color: "blue",
+    textDecorationLine: "none",
+  },
+});
